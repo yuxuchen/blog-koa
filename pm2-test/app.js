45b97f23ad4ceac6865b53fb2f1609c13b2a07d2
yuxuchen <chenyuxuan0930@gmail.com>
2021-11-18 14:45:35 +1100

    update

---
+++ pm2-test/app.js
@@ -0,0 +1,21 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    console.log('cur time', Date.now())
+    console.error('pretend to have error', Date.now())
+
+    if(req.url === '/err'){
+        throw new Error('/err this is a error!')
+    }
+    
+    res.setHeader('Content-type', 'application/json')
+    res.end(
+        JSON.stringify({
+            errno:0,
+            msg:'pm2 test server 2'
+        })
+    )
+})
+
+server.listen(8000)
+console.log('server is listening on port 8000 ...')
